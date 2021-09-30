defac94e94f1ded570fdc07e9a30233f300480b4
WAY29 <1162410187@qq.com>
2021-09-30 21:10:39 +0800

    allow multi target and target file

--- cmd/onefinger/main.go
+++ cmd/onefinger/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/WAY29/onefinger/core"
@@ -21,45 +22,46 @@ func main() {
 
 	// 定义选项
 	var (
-		target = app.StringsOpt("t target", make([]string, 0), "Target url")
-		// target     = app.StringOpt("t target", "", "Target url")
-		targetFile = app.StringOpt("tf", "", "Target url file")
-		threads    = app.IntOpt("threads", 10, "Thread number")
-		timeout    = app.IntOpt("timeout", 20, "Request timeout")
+		target      = app.StringsOpt("t target", make([]string, 0), "Target url")
+		targetFiles = app.StringsOpt("tf", make([]string, 0), "Target url file")
+		threads     = app.IntOpt("threads", 10, "Thread number")
+		timeout     = app.IntOpt("timeout", 20, "Request timeout")
 	)
 	// 定义版本
 	app.Version("v version", "onefinger 1.0.2")
 	// 命令行描述
-	app.Spec = "-v | (-t=<target>... | --tf=<targetFile>) [--threads=<threads>] [--timeout=<timeout>]"
+	app.Spec = "-v | (-t=<target> | --tf=<targetFile>)... [--threads=<threads>] [--timeout=<timeout>]"
 
 	app.Action = func() {
 		targetsSlice := make([]string, 0)
 		if len(*target) != 0 {
 			targetsSlice = append(targetsSlice, *target...)
-		} else {
-			if utils.IsFile(*targetFile) {
-				err, lineSlice := utils.ReadFileAsLine(*targetFile)
+		}
+		for _, targetFile := range *targetFiles {
+			if utils.Exists(targetFile) && utils.IsFile(targetFile) {
+				err, lineSlice := utils.ReadFileAsLine(targetFile)
 				if err != nil {
 					utils.OptionsError("Targetfile handle error: "+err.Error(), 2)
 				}
 				targetsSlice = append(targetsSlice, lineSlice...)
-			} else {
-				utils.OptionsError("Target and targetFile empty", 2)
 			}
 		}
 
+		if len(targetsSlice) == 0 {
+			utils.OptionsError("Target is empty", 2)
+		}
+
+		// 目标切片去重
+		targetsSlice = utils.RemoveDuplicateElement(targetsSlice)
+
 		// 主要运行逻辑
+		fmt.Printf("%v\n", targetsSlice)
+		os.Exit(0)
 
 		// 最大线程数为目标数量
 		if *threads > len(targetsSlice) {
 			*threads = len(targetsSlice)
 		}
-
-		// todo remove: 输出测试
-		// for _, t := range targetsSlice {
-		// 	fmt.Println("target: " + t)
-		// }
-		// fmt.Println("thread number: " + strconv.Itoa(*threads))
 
 		core.Initialize(targetsSlice, *threads, structs.RequestOptions{
 			Timeout: *timeout,
---
+++ utils/string.go
@@ -0,0 +1,14 @@
+package utils
+
+// 字符串数组去重
+func RemoveDuplicateElement(addrs []string) []string {
+	result := make([]string, 0, len(addrs))
+	temp := map[string]struct{}{}
+	for _, item := range addrs {
+		if _, ok := temp[item]; !ok {
+			temp[item] = struct{}{}
+			result = append(result, item)
+		}
+	}
+	return result
+}
