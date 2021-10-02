6a35379df7a37c9fb419a962937e739fc25c0525
WAY29 <1162410187@qq.com>
2021-10-03 00:24:25 +0800

    Update 1.2.0

--- cmd/onefinger/main.go
+++ cmd/onefinger/main.go
@@ -26,7 +26,7 @@ func main() {
 		timeout     = app.IntOpt("timeout", 20, "Request timeout")
 	)
 	// 定义版本
-	app.Version("v version", "onefinger 1.1.0")
+	app.Version("v version", "onefinger 1.2.0")
 	// 命令行描述
 	app.Spec = "-v | (-t=<target> | --tf=<targetFile>)... [--threads=<threads>] [--timeout=<timeout>]"
 
